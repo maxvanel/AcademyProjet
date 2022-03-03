2ea1b5777c971217afa68de1ac146d4ed2965bae
Assilhizaoui <73195261+Assilhizaoui@users.noreply.github.com>
2022-03-03 23:29:29 +0100

    Update TestRunner.java

--- src/test/java/com/ChanelProject/TestRunner.java
+++ src/test/java/com/ChanelProject/TestRunner.java
@@ -17,7 +17,7 @@ import cucumber.api.junit.Cucumber;
 		plugin = { "pretty", "html:target/cucumber-html-report", "json:json/cucumber.json",
 				
 				"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/Chenel.html" }, tags = {
-						("@product-makup") },
+						("@product-makup,@search-product") },
 
 		snippets = SnippetType.CAMELCASE, monochrome = true)
 public class TestRunner {
